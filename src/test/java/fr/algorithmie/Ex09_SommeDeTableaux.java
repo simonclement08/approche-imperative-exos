4f55107b6c4d2c330c9eeccaa310445241245b70
Clément SIMON <simon.clement.pierre.daniel.com>
2022-10-07 10:56:59 +0200

    Ex09 Somme de tableaux

--- src/test/java/fr/algorithmie/Ex09_SommeDeTableaux.java
+++ src/test/java/fr/algorithmie/Ex09_SommeDeTableaux.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import outils.Exercice;
 import outils.ExerciceRunner;
 import outils.Question;
+import outils.Resultat;
 
 /**
  * Ne modifiez ni les noms des classes, ni les noms des méthodes.
@@ -27,11 +28,11 @@ public class Ex09_SommeDeTableaux {
 	@Test
 	@Question(numero = 1)
 	public void calculSomme() {
-
-		// TODO Créer un tableau somme de même taille que les tableaux précédents et
-		// dont chaque case d’index i contient la somme arithmétique des cases d’index i
-		// des tableaux 1 et 2.
-		// Exemple : somme[i]=tab1[i]+tab2[i]
-	
+		int[] somme = new int[tab1.length];
+		
+		for (int i=0; i<somme.length; i++) {
+			somme[i] = tab1[i] + tab2[i];
+			Resultat.log(somme[i]);
+		}
 	}
 }
