4eae89f773ecfd42ff7440d31ea1c683d7f8caef
Clément SIMON <simon.clement.pierre.daniel.com>
2022-10-07 11:40:36 +0200

    Ex11 First Last 6

--- src/test/java/fr/algorithmie/Ex11_FirstLast6.java
+++ src/test/java/fr/algorithmie/Ex11_FirstLast6.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import outils.Exercice;
 import outils.ExerciceRunner;
 import outils.Question;
+import outils.Resultat;
 
 /**
  * Ne modifiez ni les noms des classes, ni les noms des méthodes.
@@ -31,14 +32,20 @@ public class Ex11_FirstLast6 {
 	@Test
 	@Question(numero = 1)
 	public void afficherAlgo() {
-
-		// TODO Calculez une valeur booléenne qui contrôle le tableau de la sorte :
-		//  o elle vaut true si le tableau a au moins 1 élément et si le premier élément ou le dernier élément vaut 6.
-		//  o elle vaut false dans les autres cas
-
-
-		// TODO LOGUEZ la valeur obtenue pour chacun des 6 tableaux tab1 à tab6
-
+		boolean check = false;
+		
+		check = tab1.length >= 1 && (tab1[0] == 6 || tab1[tab1.length-1] == 6);
+		Resultat.log(check);
+		check = tab2.length >= 1 && (tab2[0] == 6 || tab2[tab2.length-1] == 6);
+		Resultat.log(check);
+		check = tab3.length >= 1 && (tab3[0] == 6 || tab3[tab3.length-1] == 6);
+		Resultat.log(check);
+		check = tab4.length >= 1 && (tab4[0] == 6 || tab4[tab4.length-1] == 6);
+		Resultat.log(check);
+		check = tab5.length >= 1 && (tab5[0] == 6 || tab5[tab5.length-1] == 6);
+		Resultat.log(check);
+		check = tab6.length >= 1 && (tab6[0] == 6 || tab6[tab6.length-1] == 6);
+		Resultat.log(check);
 	}
 
 }
