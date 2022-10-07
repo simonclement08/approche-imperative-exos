1ece4c137eaecdf04b95f460319d2fdb10026a85
Clément SIMON <simon.clement.pierre.daniel.com>
2022-10-07 11:56:55 +0200

    Ex13 Rotation

--- src/test/java/fr/algorithmie/Ex13_Rotation.java
+++ src/test/java/fr/algorithmie/Ex13_Rotation.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import outils.Exercice;
 import outils.ExerciceRunner;
 import outils.Question;
+import outils.Resultat;
 
 /**
  * Ne modifiez ni les noms des classes, ni les noms des méthodes.
@@ -27,12 +28,14 @@ public class Ex13_Rotation {
 	@Test
 	@Question(numero = 1)
 	public void afficherAlgo() {
-
-		// TODO Effectuez une rotation à droite des éléments du tableau array
-		// Exemple : si le tableau contient {0,1,2,3} alors il contiendra {3,0,1,2}
-		// après rotation
-		
-		// TODO Afficher le contenu du tableau après rotation avec Resultat.log
+		int last = array[array.length-1];
+		for (int i=array.length-1; i>0; i--) {
+			array[i] = array[i-1];
+		}
+		array[0] = last;
+		for (int i=0; i<array.length; i++) {
+			Resultat.log(array[i]);
+		}
 	}
 
 }
