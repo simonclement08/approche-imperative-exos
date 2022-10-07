6039affbce1e65a74daa6e22f3b8eb8b352fd408
Clément SIMON <simon.clement.pierre.daniel.com>
2022-10-07 12:04:30 +0200

    Ex15 Interactif Somme Arithmetique

--- src/test/java/fr/algorithmie/Ex15_InteractifSommeArithmetique.java
+++ src/test/java/fr/algorithmie/Ex15_InteractifSommeArithmetique.java
@@ -28,13 +28,14 @@ public class Ex15_InteractifSommeArithmetique {
 	public void interactif() {
 		
 		Scanner scanner = new Scanner(System.in) ;
-		
-		// TODO Demandez un nombre à l'utilisateur puis LOGUEZ ce nombre 
-		// avec Resultat.log
-		
-		
-		// TODO LOGUEZ avec Resultat.log la somme de 1 à ce nombre inclus
-		
+		System.out.println("Veuillez saisir un nombre : ");
+		int nb = scanner.nextInt();
+		Resultat.log(nb);
+		int sum = 0;
+		for (int i=1; i<nb+1; i++) {
+			sum += i;
+		}
+		Resultat.log(sum);
 		
 		scanner.close();
 	}
