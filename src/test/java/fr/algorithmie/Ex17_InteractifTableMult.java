d09261f8c77e85338ed8010d493695c4c444f961
Clément SIMON <simon.clement.pierre.daniel.com>
2022-10-07 12:50:32 +0200

    Ex17 Interactif Table Mult

--- src/test/java/fr/algorithmie/Ex17_InteractifTableMult.java
+++ src/test/java/fr/algorithmie/Ex17_InteractifTableMult.java
@@ -8,6 +8,7 @@ import org.junit.runner.RunWith;
 import outils.Exercice;
 import outils.ExerciceRunner;
 import outils.Question;
+import outils.Resultat;
 
 /**
  * Ne modifiez ni les noms des classes, ni les noms des méthodes.
@@ -28,12 +29,14 @@ public class Ex17_InteractifTableMult {
 	public void interactif() {
 		
 		Scanner scanner = new Scanner(System.in) ;
-		
-		// TODO Tant que le nombre saisi par l'utilisateur n'est pas compris
-		// entre 1 et 10 l'application continue à demander un nombre à l'utilisateur.
-		
-		// Si ce nombre est compris entre 1 et 10, l'application affiche la table de multiplication
-		// de ce nombre sous la forme : 3*1=3, 3*2=6, etc... jusqu'à 3*2=10
+		System.out.println("Veuillez saisir un nombre : ");
+		int nb = scanner.nextInt();
+		while (nb < 1 || nb > 10) {
+			nb = scanner.nextInt();
+		}
+		for (int i = 1; i <= 10; i++) {
+			Resultat.log(nb + "*" + i + "=" + nb*i);
+		}
 		
 		scanner.close();
 	}
