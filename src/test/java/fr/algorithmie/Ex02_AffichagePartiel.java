8972e86e4ca2f26808f440e869426ac2b9b6aae3
Clément SIMON <simon.clement.pierre.daniel.com>
2022-10-07 09:51:41 +0200

    Ex02 Affichage Partiel

--- src/test/java/fr/algorithmie/Ex02_AffichagePartiel.java
+++ src/test/java/fr/algorithmie/Ex02_AffichagePartiel.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import outils.Exercice;
 import outils.ExerciceRunner;
 import outils.Question;
+import outils.Resultat;
 
 /** Ne modifiez ni les noms des classes, ni les noms des méthodes.
  * Utilisez Resultat.log pour afficher les résultats
@@ -22,30 +23,40 @@ public class Ex02_AffichagePartiel {
 	@Test
 	@Question(numero=1)
 	public void affichageValeursSup3() {
-		
-		//TODO En utilisant une boucle et un if, LOGUER avec Resultat.log 
-		// les valeurs du tableau supérieures ou égales à 3
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] >= 3) {
+				Resultat.log(array[i]);
+			}
+		}
 	}
 	
 	@Test
 	@Question(numero=2)
 	public void affichageValeursPaires() {
-		
-		//TODO LOGUER avec Resultat.log les valeurs paires du tableau (0 sera considéré comme pair)
+		for (int i = 0; i < array.length; i++) {
+			if (array[i]%2 == 0) {
+				Resultat.log(array[i]);
+			}
+		}
 	}
 	
 	@Test
 	@Question(numero=3)
 	public void affichageIndexPairs() {
-		
-		//TODO LOGUER avec Resultat.log les valeurs correspondant aux index pairs du tableau 
-		// (0 sera considéré comme pair)
+		for (int i = 0; i < array.length; i++) {
+			if (i%2 == 0) {
+				Resultat.log(array[i]);
+			}
+		}
 	}
 	
 	@Test
 	@Question(numero=4)
 	public void affichageValeursImpaires() {
-		
-		//TODO LOGUER avec Resultat.log les valeurs impaires du tableau (0 doit être exclu)
+		for (int i = 0; i < array.length; i++) {
+			if (array[i]%2 == 1) { // Remplacer array[i]%2 par Math.abs(array[i]%2) pour prendre en compte les valeurs négatifs
+				Resultat.log(array[i]);
+			}
+		}
 	}
 }
