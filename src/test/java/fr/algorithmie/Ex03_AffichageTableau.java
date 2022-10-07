b0b92c2bb39d63d5fc5e59243025573a410153c0
Clément SIMON <simon.clement.pierre.daniel.com>
2022-10-07 09:58:10 +0200

    Ex03 Affichage Tableau

--- src/test/java/fr/algorithmie/Ex03_AffichageTableau.java
+++ src/test/java/fr/algorithmie/Ex03_AffichageTableau.java
@@ -1,11 +1,14 @@
 package fr.algorithmie;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import outils.Exercice;
 import outils.ExerciceRunner;
 import outils.Question;
+import outils.Resultat;
 
 /** Ne modifiez ni les noms des classes, ni les noms des méthodes.
  * Utilisez Resultat.log pour afficher les résultats
@@ -22,14 +25,16 @@ public class Ex03_AffichageTableau {
 	@Test
 	@Question(numero=1)
 	public void affichageTableau() {
-		
-		//TODO LOGUER les éléments du tableau grâce à une boucle
+		for (int i=0; i<array.length; i++) {
+			Resultat.log(array[i]);
+		}
 	}
 	
 	@Test
 	@Question(numero=2)
 	public void affichageTableauOrdreInverse() {
-		
-		//TODO LOGUER les éléments du tableau dans l'ordre inverse
+		for (int i = array.length - 1; i >= 0; i--) {
+			Resultat.log(array[i]);
+		}
 	}
 }
